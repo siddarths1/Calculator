596240ce56e26387f9987a29bfeecdfb8e121e0f
siddarths1 <siddarths.20cse@kongu.edu>
2021-10-29 19:10:17 +0530

    multiplication

---
+++ calculate.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	int b;
+	printf("enter num 1");
+	scanf("%d",&a);
+	printf("enter num 2");
+	scanf("%d",&b);
+	printf("enter for multiplication");
+	int mult = a*b;
+	printf("multiplication is ",mult);
+}
